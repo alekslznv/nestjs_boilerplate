aa20b739d950f9fd721c21780d0b775eba73a908
aleksandr.seleznev <aleksandr.seleznev24@gmail.com>
2026-03-10 10:34:33 +0100

    Add ValdiationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableShutdownHooks();
   app.enableVersioning({
     type: VersioningType.URI,
